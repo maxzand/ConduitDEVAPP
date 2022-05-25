1b28e3b742f149a0203125d0b4bfbaf6d78fc7e0
mazime <mailmaxzand@gmail.com>
2022-05-24 21:41:08 -0700

    Added MVVM toolkit and button onto connectionview

---
+++ ViewModels/ConnectionViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ConduitDEVAPP.ViewModels
+{
+    public partial class ConnectionViewModel : ObservableObject
+    {
+       
+
+        
+
+
+
+
+    }
+}
