b785a8c465a5b2701ac4d9eb3688f3eaf964d923
mazime <mailmaxzand@gmail.com>
2022-05-28 03:31:09 -0700

    Created singleton to manage all bluetooth buisness logic

---
+++ Models/BluetoothManagerSingleton.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConduitDEVAPP.Models
+{
+    public sealed class BluetoothManagerSingleton
+    {
+
+        #region Singleton Declaration
+        private static readonly BluetoothManagerSingleton _instance = new BluetoothManagerSingleton();
+        // Private constructor as this is a singleton
+        private BluetoothManagerSingleton()
+        {
+
+        }
+        public static BluetoothManagerSingleton Instance
+        {
+            get { return _instance; }
+        }
+        #endregion
+
+        private string savedname = "Max"; // Saved device name from settings.
+
+    }
+}
