d0827fa6785a958f348315c5b6e9134ba365701e
AnthonyRodi <acrodi@sundevils.asu.edu>
2022-06-19 01:21:19 -0700

    added toBinary

--- Models/AppendixHelper.cs
+++ Models/AppendixHelper.cs
@@ -6,11 +6,18 @@ using System.Threading.Tasks;
 
 namespace ConduitDEVAPP.Models
 {
-    internal class AppendixHelper
+    public sealed class AppendixHelper
     {
         public string[] categoryIDHelper = new string[] { "CategoryIDOther", "CategoryIDIncomingCall", "CategoryIDMissedCall", "CategoryIDVoicemail", "CategoryIDSocial", "CategoryIDSchedule", "CategoryIDEmail", "CategoryIDNews", "CategoryIDHealthAndFitness", "CategoryIDBusinessAndFinance", "CategoryIDLocation", "CategoryIDEntertainment", "ReservedCategoryIDValues" };
         public string[] eventIDHelper = new string[] { "EventIDNotificationAdded", "EventIDNotificationModified", "EventIDNotificationRemoved", "ReservedEventIDValues" };
-
-    }
+           
+        public byte[] toBinary(int int1)
+        {
+            byte[] result = BitConverter.GetBytes(int1);
+            return result;
+        }
         
+    }
+    
+
 }
